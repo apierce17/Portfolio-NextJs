a8a9e9aafc7981477d9923246422b11eff01fdba
Ashton Pierce <ashton@myeyeris.com>
2022-10-10 10:16:09 -0500

    Update projects

--- data/projects.tsx
+++ data/projects.tsx
@@ -3,7 +3,7 @@ import { FiGithub } from "react-icons/fi";
 export const Projects = [
   {
     title: "Eyeris",
-    languages: ['React', 'NextJS', 'Git', 'javascript', 'typescript', 'photoshop', 'figma', 'adobexd', 'php', 'aws', 'mysql', 'npm', 'redux'],
+    languages: ['React', 'NextJS', 'javascript', 'typescript', 'photoshop', 'figma', 'adobexd', 'php', 'aws', 'mysql', 'npm', 'redux'],
     github: "",
     deployed: "https://www.myeyeris.com",
     description: "Eyeris is a contact lense company that aims to support the doctor / patient relationship.",
@@ -19,7 +19,7 @@ export const Projects = [
     deployed: "https://www.mercantilecentergarage.com/",
     description: "A parking garage website that allows customers to easily reserve parking.",
     imageName: "mcg",
-    order: 5,
+    order: 6,
     date: "Q3 2021",
     type: 'Work'
   },
@@ -30,29 +30,29 @@ export const Projects = [
     deployed: "https://www.soldierfieldparking.com/",
     description: "A parking garage website that allows customers to easily reserve parking.",
     imageName: "sf",
-    order: 6,
+    order: 7,
     date: "Q2 2021",
     type: 'Work'
   },
   {
     title: "Steam Tools",
-    languages: ['javascript', 'photoshop'],
-    github: "https://github.com/apierce17/steam-tools",
-    deployed: "https://apierce17.github.io/steam-tools/",
+    languages: ['javascript', 'nextjs', 'react', 'npm', 'typescript'],
+    github: "https://github.com/apierce17/steam-tools-nextjs",
+    deployed: "https://steam-tools.vercel.app",
     description: "Compare Steam games with friends, or generate a random steam item!",
     imageName: "st",
-    order: 7,
-    date: "Q3 2021",
+    order: 3,
+    date: "Q3 2022",
     type: 'Personal'
   },
   {
     title: "Product Image Generator",
     languages: ['javascript', 'php', 'photoshop'],
-    github: "Available On Request",
+    github: "",
     deployed: "",
     description: "Generate mass quantities of product images with less than 10 clicks! A demo or code review is available on request.",
     imageName: "",
-    order: 3,
+    order: 4,
     date: "Q2 2022",
     type: 'Personal'
   },
@@ -63,7 +63,7 @@ export const Projects = [
     deployed: "https://musicians-mecca.herokuapp.com/",
     description: "Easily find music teachers or students that match your instrument and play style.",
     imageName: "mm",
-    order: 8,
+    order: 9,
     date: "Q1 2021",
     type: 'School'
   },
@@ -74,7 +74,7 @@ export const Projects = [
     deployed: "https://apierce17.github.io/NashvilleMusicEvents/",
     description: "Find music events and lodging in Nashville, TN.",
     imageName: "nme",
-    order: 9,
+    order: 10,
     date: "Q1 2021",
     type: 'School'
   },
@@ -85,7 +85,7 @@ export const Projects = [
     deployed: "https://master.d3n94dl6rxvb27.amplifyapp.com/",
     description: "Find Github repositiories of companies and look through their commits!",
     imageName: "",
-    order: 4,
+    order: 5,
     date: "Q1 2022",
     type: 'Personal'
   },
